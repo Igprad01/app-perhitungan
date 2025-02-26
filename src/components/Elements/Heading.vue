8f6b4e826f0460c3ea4f99c1ffe245d2f19b234f
igprad01 <igustiputra99@gmail.com>
2025-02-26 21:43:56 +0700

    add heading  1-6

---
+++ src/components/Elements/Heading.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  level: {
+    type: Number,
+    default: 1,
+  },
+
+  text: {
+    type: String,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <div>
+    <component
+      :is="`h${level}`"
+      :class="
+        {
+          1: 'text-4xl',
+          2: 'text-3xl',
+          3: 'text-2xl',
+          4: 'text-xl',
+          5: 'text-lg',
+          6: 'text-base',
+        }[level]
+      "
+      >{{ text }}</component
+    >
+  </div>
+</template>
