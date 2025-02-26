968013e366501ceb93aaa6294cc07421ffb7cb3f
igprad01 <igustiputra99@gmail.com>
2025-02-26 21:44:15 +0700

    add P

---
+++ src/components/Elements/Teks.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  modelValue: [String, Number],
+  Type: {
+    type: String,
+    default: 'text',
+  },
+  Paragraf: String,
+})
+</script>
+
+<template>
+  <div>
+    <p>{{ Paragraf }}</p>
+  </div>
+</template>
