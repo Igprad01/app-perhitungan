8e8c91146170c2ee3258089c67b33e71c7c769f8
igprad01 <igustiputra99@gmail.com>
2025-03-06 17:10:15 +0700

    custom props agar value input compatible props

--- src/components/Elements/InputField.vue
+++ src/components/Elements/InputField.vue
@@ -10,10 +10,22 @@ defineProps({
   teksInput: String,
   classname: String,
 })
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = (event) => {
+  emit('update:modelValue', event.target.value)
+}
 </script>
 
 <template>
   <div>
-    <input :type="type" :placeholder="teksInput" :class="classname" />
+    <input
+      :type="type"
+      :placeholder="teksInput"
+      :class="classname"
+      :value="modelValue"
+      @input="updateValue"
+    />
   </div>
 </template>
