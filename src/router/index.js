e507453c3138f00124ca20e9b445cce01bd3a243
igprad01 <igustiputra99@gmail.com>
2025-03-07 14:02:15 +0700

    tambah font dan fix design UI

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/Splitbill.vue'
+import Home from '../views/splitBill.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Spilbill',
-      component: HomeView,
+      name: 'Home',
+      component: Home,
     },
   ],
 })
--- src/views/SplitBill.vue
+++ src/views/SplitBill.vue
@@ -1,77 +1,63 @@
 <script setup>
-// import components
-import InputField from '@/components/Elements/InputField.vue'
 import Heading from '@/components/Elements/Heading.vue'
+import InputField from '@/components/Elements/InputField.vue'
 import Button from '@/components/Elements/Button.vue'
 import { ref } from 'vue'
 
-// fungsi menghitung splitbill
 const TotalPesanan = ref('')
 const TotalTeman = ref('')
 const Diskon = ref('')
-const Ppn = ref('')
-const HasilData = ref('')
+const PPN = ref('')
+const Hasil = ref('')
 
-const HitungPatungan = () => {
-  // deklarasi variable
+const ListInput = ref([
+  { Teks: 'Total Pesanan', hasil: TotalPesanan, tipe: 'number' },
+  { Teks: 'Jumlah Teman', hasil: TotalTeman },
+  { Teks: 'Total Diskon', hasil: Diskon },
+  { Teks: 'Pajak Pesanan', hasil: PPN },
+])
+
+const HitungHasil = () => {
   let Total = parseFloat(TotalPesanan.value)
   let JumlahTeman = parseInt(TotalTeman.value)
   let TotalDiskon = parseFloat(Diskon.value)
-  let TotalPpn = parseFloat(Ppn.value)
-  // perhitungan nya
+  let Ppn = parseFloat(PPN.value)
+
+  // perhitungan
+
   let TotalSemuanya = Total - (Total * TotalDiskon) / 100
-  let AfterPPN = TotalSemuanya + (TotalSemuanya * TotalPpn) / 100
-  let BiayaOrang = AfterPPN / JumlahTeman
+  let sesudahPPN = TotalSemuanya + (TotalSemuanya * Ppn) / 100
+  let BiayaOrang = sesudahPPN / JumlahTeman
   let totalPembayaran = BiayaOrang * JumlahTeman
 
-  HasilData.value = `Total Pesanan: ${Total}\nJumlah Teman: ${JumlahTeman}\nDiskon: ${TotalDiskon}%\nPPN: ${TotalPpn}%\nTotal Setelah Diskon: ${TotalSemuanya}\nTotal Setelah PPN: ${AfterPPN}\nBiaya Per Orang: ${BiayaOrang}\nTotal Pembayaran: ${totalPembayaran}`
+  Hasil.value = `total pesanan: ${Total}, total pesanan: ${Total}${JumlahTeman}, ${TotalDiskon},${Ppn}, ${TotalSemuanya}, ${BiayaOrang}, ${totalPembayaran}`
 }
+
+// tinggal fix font dan tinggal fix proses perhitungan
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-svh md:min-h-svw lg:min-h-screen font-doto">
+  <div class="flex items-center justify-center min-h-screen font-Red-Mono">
     <div
-      class="w-full border-hidden box-border max-w-8/10 bg-slate-200 rounded shadow-[5px_3px_0px_0px_rgba(0,_0,_0,_0.95)] p-6"
+      class="w-full border box-border max-w-7/10 bg-slate-300 p-6 shadow-[8px_5px_0px_0px_rgba(0,_0,_0,_0.95)]"
     >
-      <div class="mx-auto">
-        <Heading
-          :level="3"
-          text="- App patungan - "
-          Classname=" text-center font-bold uppercase mb-5"
+      <Heading text="--APP PATUNGAN--" level="5" Classname="font-bold uppercase text-center mb-5" />
+      <InputField
+        v-for="(item, index) in ListInput"
+        :key="index"
+        :teksInput="item.Teks"
+        v-model="item.hasil"
+        classname="w-full mb-4 py-3 px-3 border rounded-md border-black text bg-slate-100 text-sm transition
+        duration-200 focus:outline-2 shadow-lg focus:shadow"
+      />
+      <div class="flex items-center justify-center mt-5">
+        <Button
+          Teks="Hitung Patungan"
+          @click="HitungHasil"
+          Classname="px-4 py-4 bg-amber-500 outline shadow-[8px_5px_0px_0px_rgba(0,_0,_0,_0.95)]"
         />
-        <InputField
-          teksInput="Total Pesanan"
-          v-model="TotalPesanan"
-          type="number"
-          classname="w-full bg-transparent mb-4 py-3 px-3 placeholder:text-black border border-dashed rounded-md border-black text-slate-900 text-sm transition duration-100 focus:outline-dashed shadow-md focus:shadow"
-        />
-        <InputField
-          teksInput="Jumlah Teman"
-          v-model="TotalTeman"
-          type="number"
-          classname="w-full bg-transparent mb-4 py-3 px-3 placeholder:text-black border border-dashed rounded-md border-black text-slate-900 text-sm transition duration-100 focus:outline-dashed shadow-md focus:shadow"
-        />
-        <InputField
-          teksInput="Diskon"
-          v-model="Diskon"
-          type="number"
-          classname="w-full bg-transparent mb-4 py-3 px-3 placeholder:text-black border border-dashed rounded-md border-black text-slate-900 text-sm transition duration-100 focus:outline-dashed shadow-md focus:shadow"
-        />
-        <InputField
-          teksInput="PPN"
-          v-model="Ppn"
-          type="number"
-          classname="w-full bg-transparent mb-4 py-3 px-3 placeholder:text-black border border-dashed rounded-md border-black text-slate-900 text-sm transition duration-100 focus:outline-dashed shadow-md focus:shadow"
-        />
-        <div class="flex items-center justify-center min--screen mt-5">
-          <Button
-            Teks="ini adalah button"
-            @click="HitungPatungan"
-            Classname="px-3 py-3 border border-black outline-2 bg-amber-300  shadow-[8px_5px_0px_0px_rgba(0,_0,_0,_0.95)]"
-          />
-        </div>
-        <textarea v-model="HasilData" cols="30" rows="10" class="w-full border p-2 mt-4"></textarea>
       </div>
+      <textarea v-model="Hasil" class="border w-full mt-5" cols="50" rows="5" disabled></textarea>
     </div>
   </div>
 </template>
