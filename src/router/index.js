6724c67aff6271cf357c17a82e74b992d9f09cef
igprad01 <igustiputra99@gmail.com>
2025-03-10 14:33:34 +0700

    fix router @

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/splitBill.vue'
+import Home from '@/views/splitBill.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
